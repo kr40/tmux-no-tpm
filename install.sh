327d4b058d6db0134236eb00357cf82e98d6a285
Kartik Rao <kr40@anon>
2022-08-04 13:01:06 +0530

    Added Files

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Title: kr40's TMUX-Config
+# Description: Adds useful aliases to zshrc in kali
+# Author: Kartik Rao <kr40.git@gmail.com>
+# Date: 2022-08-04
+# Version: 1.0.0
+
+function bannerSimple() {
+  local msg="${2} ${1} ${2}"
+  local edge
+  edge=${msg//?/$2}
+  echo "${edge}"
+  echo "$(tput bold)${msg}$(tput sgr0)"
+  echo "${edge}"
+  echo
+}
+
+bannerSimple "kr40's TMUX Config" "*"
+
+echo Adding .tmux.conf
+
+cp .tmux.conf ~/.tmux.conf
+
+echo Adding tat
+
+cp tat.sh ~/tat.sh
+chmod a+x ~/tat.sh
+ln -sf ~/tat.sh /usr/bin/tat
+
+echo Adding Aliases and Functions
+
+cat aliases.txt >> ~/.profile
+
+echo Please Restart You Terminal
+
+echo Installation Finished! Enjoy..
